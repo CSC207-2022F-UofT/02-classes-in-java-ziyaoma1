c3e54fcb84adcf5ea9711355384038a3a8fbffb2
ziyaoma1 <113070856+ziyaoma1@users.noreply.github.com>
2022-09-10 13:18:07 -0400

    Add files via upload

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -79,6 +79,9 @@ public class Silly implements Comparable<Silly>{
      *       strings (e.g. this.name = [first string] + [second string]).
      *       Make sure you document this method!
      */
+    public Silly(String a, String b){
+        this.name = a + b;
+    }
 
 
 
@@ -116,7 +119,7 @@ public class Silly implements Comparable<Silly>{
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {};
+        int[] expected_values = {0, 1, 2, 3};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
@@ -134,6 +137,7 @@ public class Silly implements Comparable<Silly>{
     @Override
     public String toString(){
         // TODO (Task 3): Implement the body of this method!
+        return this.name;
     }
 
     /**
@@ -156,12 +160,19 @@ public class Silly implements Comparable<Silly>{
         if (!(o instanceof Silly)){
             return false;
         }
+        if (o == null){
+            return false;
+        }
 
         Silly other = (Silly) o; // To access .name of o, we need to cast it.
+        if (this.toString().equals(o.toString())){
+            return true;
+        }
+        return false;
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
-    }
+     }
 
     /**
      * 5. If we want to compare two objects, we have to do two things:
@@ -194,7 +205,15 @@ public class Silly implements Comparable<Silly>{
          *                You can get the length of a string by using the
          *                .length() method.
          */
+        if (this.toString().length() > other.toString().length()) {
+            return 1;
+        }
+        if (this.toString().length() < other.toString().length()) {
+            return (-1);
+        }
+        return 0;
     }
+
 
     /*
      * TODO (Task 6): Submit the changes you made on GitHub!
